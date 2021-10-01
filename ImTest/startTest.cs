707a08882f9efadaf9859c9ad5e1a9cc5797a1fd
MaxEKB03 <maxekb@live.ru>
2021-10-01 23:18:12 +0500

    Добавьте файлы проекта.

---
+++ ImTest/startTest.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace ImTest
+{
+    class startTest
+    {
+        static Dictionary<string, string> openConfig(Dictionary<string, string> config) // доступ тип_вывода название (переменные)
+        {
+            // чтение, запись файла
+            string[] configFile;
+            configFile = System.IO.File.ReadAllLines("config.txt");
+            foreach (string param in configFile)
+            {
+                string[] forDict = param.Split();
+                config.Add(forDict[0], forDict[1]);
+            }
+            return config;
+        }
+        static void changeConsole(Dictionary<string, string> config) // void - без вывода
+        {
+            // чтение конфига
+            ConsoleColor ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), config["ForegroundColor"]);
+            //   тип        название            тип      класс|конверт  в       тип        словарь     раздел
+            ConsoleColor BackgroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), config["BackgroundColor"]);
+            // enum - класс для перечислений
+
+            // применение конфигураций
+            Console.Title = config["Title"];
+            Console.CursorVisible = bool.Parse(config["CursorVisible"]);
+            Console.BackgroundColor = BackgroundColor;
+            Console.ForegroundColor = ForegroundColor;
+            Console.Clear();
+        }
+        static string findUnit(string pathOfDirectory)
+        {
+            // запрос нужного раздела
+            DirectoryInfo dir = new DirectoryInfo(pathOfDirectory);
+            Console.WriteLine("Номер файла, который хотите открыть для тестирования?");
+            int counter = 1;
+            Dictionary<int, string> filesForTest = new Dictionary<int, string>();
+
+            // вылавливание ошибок
+
+            try // проверка на конфиг
+            {
+                foreach (var item in dir.GetFiles())
+                {
+                    Console.WriteLine($"{counter}. {item.Name}");
+                    filesForTest.Add(counter, item.Name);
+                    counter += 1;
+                }
+                try // проверка запроса
+                {
+                    int req = Convert.ToInt32(Console.ReadLine());
+                    try // проверка на наличае файла
+                    {
+                        string pathOfUnit = filesForTest[req];
+                        return pathOfUnit;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Нет такого файла");
+                        return findUnit(pathOfDirectory);
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Вы ввели неверное значение");
+                    return findUnit(pathOfDirectory);
+                }
+            } catch (Exception) {
+                Console.WriteLine("[EROR] отредактируте файл config.txt, укажите нужный путь к разделу с лескикой.");
+                Console.ReadKey();
+                Process.GetCurrentProcess().Kill();
+                return null;
+            }
+            
+        }
+        public void reInit()
+        {
+            //инициализация повторного запуска
+
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("Запустить программу еще раз? y/n");
+            string req = Console.ReadLine();
+            if (req == "y" || req == "Y")
+            { // запуск опять
+                Main();
+            } else { // закрытие
+                System.Diagnostics.Process.GetCurrentProcess().Kill();
+            }
+        }
+        static void Main()
+        {
+            // объявление перменных
+            Dictionary<string, string> config = new Dictionary<string, string>(); // - файл с настройками
+            test test = new test(); // - ссылка на объект test
+            string pathOfUnit; // путь к лексике
+
+            //запуск методов
+            config = openConfig(config); // чтение config.txt
+            changeConsole(config); // изменение консосли по конфигу
+            pathOfUnit = findUnit(config["pathOfDirectory"]); // присваивание пути к лексике
+            Console.WriteLine(pathOfUnit); 
+            test.init($"{config["pathOfDirectory"]}/{pathOfUnit}"); // запуск теста
+        }
+    }
+}
---
+++ ImTest/test.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ImTest
+{
+    class test
+    {
+        startTest startTest = new startTest();
+        public void init(string path)
+        {
+            // инициализация
+            string[] file = System.IO.File.ReadAllLines(path);
+            Dictionary<string, string> words_rus_eng = new Dictionary<string, string>();
+            
+            // получение рандомных значений
+            var rand = new Random();
+            var nums = Enumerable.Range(0, file.Length).OrderBy(n => rand.Next()).ToArray();
+            try // проверка лексического файла на правильность ввода
+            {
+                foreach (int id in nums)
+                {
+                string[] forDict = file[id].Split();
+                words_rus_eng.Add(forDict[0], forDict[1]);
+                }
+            } catch (Exception) {
+                Console.WriteLine("[EROR] выбранный файл не содержит лекскиу, или не правильного формата");
+                Console.WriteLine("Файл с лекский ввида [русское слово][пробел][английское слово]");
+                startTest.reInit();
+            }
+
+            // запуск теста
+            exam(words_rus_eng);
+        }
+        public void exam(Dictionary<string, string> words)
+        {
+            // начало
+            Console.WriteLine("Начнём тест");
+            Console.WriteLine("Начните вводить перевод русских слов на английском");
+            Console.WriteLine("Нажмите любую кнопку для продолжения");
+            Console.ReadKey();
+
+            // ввод значений
+            Dictionary<string, string> result = new Dictionary<string, string>();
+            Console.WriteLine();
+            foreach (var word in words)
+            {
+                Console.Write($"{word.Key} : ");
+                string answer = Console.ReadLine(); // проверка на правильность ответа
+                if (answer == word.Value) // запись для сводки
+                {
+                    result.Add($"{word.Key} - {answer}", "correct");
+                } else {
+                    result.Add($"{word.Key} - {answer} {word.Value}", "uncorrect");
+                }   
+            }
+
+            // конец
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Black;
+            foreach (string line in result.Keys)
+            {
+                if (result[line] == "correct")
+                {
+                    Console.BackgroundColor = ConsoleColor.Green;
+                } else {
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                }
+                Console.WriteLine(line);
+            }
+            startTest.reInit();
+        }
+    }
+}
